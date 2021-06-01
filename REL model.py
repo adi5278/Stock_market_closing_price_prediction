e55dbd5d36b7166aa71bb3604ba341cbbba85667
Shubham Singh <shubhamomprakashsingh@gmail.com>
2021-06-01 18:30:16 +0530

    Add files via upload

---
+++ REL model.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score,mean_squared_error
+
+
+#Data Preprocessing -- Turning data into required useful data
+
+df = pd.read_csv("C:\\Users\\user\\Desktop\\Projects\\Minor Project - 2\\Dataset\RELIANCE.BO.csv")
+df= df[df.Volume!=0]
+df = df.reset_index(drop=True)
+df = df.drop(np.where(np.isnan(df['Volume'].to_numpy().astype('float32')))[0])
+df = df.reset_index(drop=True)
+
+rows = df.shape[0]
+columns = df.shape[1]
+
+df["Open-Close"] = df["Open"] - df["Close"]   # Open - Close
+df["High-Low"] = df["High"] - df["Low"]       # High - Low
+
+# 7 day Moving Average
+df["7DMA"]=0.000000
+for i in range(6,rows):
+    df["7DMA"][i] = (df["Close"][i]+df["Close"][i-1]+df["Close"][i-2]+df["Close"][i-3]+df["Close"][i-4]+df["Close"][i-5]+df["Close"][i-6])/7
+
+#21 Day Moving Average
+df["21DMA"]=0.000000
+for i in range(20,rows):
+    df["21DMA"][i] = (df["Close"][i]+df["Close"][i-1]+df["Close"][i-2]+df["Close"][i-3]+df["Close"][i-4]+df["Close"][i-5]+df["Close"][i-6]+df["Close"][i-7]+df["Close"][i-8]+df["Close"][i-9]+df["Close"][i-10]+df["Close"][i-11]+df["Close"][i-12]+df["Close"][i-13]+df["Close"][i-14]+df["Close"][i-15]+df["Close"][i-16]+df["Close"][i-17]+df["Close"][i-18]+df["Close"][i-19]+df["Close"][i-20])/21
+    
+# 7 day Standard Deviation
+import math
+df["7DSD"]=0.000000
+for i in range(6,rows):
+    sum = 0.000000
+    for j in range(7):
+        sum = sum + (df["Close"][i-j] - df["7DMA"][i])**2
+    df["7DSD"][i] = math.sqrt(sum/7)
+
+# Next day Closing Price
+# df = df[df.Date!="2021-04-30"]
+ndf = df.copy()
+ndf = ndf.drop(0)
+ndf = ndf.reset_index(drop=True)
+df["Next"] = ndf.Close
+# df["Next"][6348] = 1994.45
+
+df = df.drop(columns=['Open','High','Close','Adj Close','Low'])
+ndf = df[df['21DMA']!=0]
+ndf = ndf.drop(rows-1)
+ndf = ndf.reset_index(drop=True)
+
+
+# Forming Dependent and Independent variables x and y
+
+X = ndf[['Date','Volume','Open-Close','High-Low','7DMA','21DMA','7DSD']].to_numpy()
+y = ndf['Next'].to_numpy()
+x = X[:,1:]
+
+
+
+# Data Splitting into Training and Testing data
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
+x_train = X_train[:,1:]
+x_test = X_test[:,1:]
+
+# Regression Model
+
+regressor = RandomForestRegressor(n_estimators = 1000, random_state = 0)
+regressor.fit(x_train, y_train)
+
+
+#saving model to disk
+import pickle
+
+pickle.dump(regressor,open('C:\\Users\\user\\Desktop\\Projects\\Minor Project - 2\\Deployment\REL model.pkl','wb'))
+
+
---
+++ app.py
@@ -0,0 +1,118 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import requests
+import math
+from datetime import date
+
+appp= Flask(__name__)
+@appp.route('/')
+
+def home():
+	return render_template('index.html')
+
+
+@appp.route('/predict', methods=['POST'])
+
+def predict():
+	try:
+		if(request.form.get(["Stock"][0])=='REL'):
+
+			params = {
+				'function': 'TIME_SERIES_DAILY',
+				'symbol': 'RELIANCE.BSE',
+				'outputsize' :'full',
+				'apikey' : 'SD7E3STMW2WO613H'
+				#   'query': 'New York'
+			}
+
+			api_result = requests.get('https://www.alphavantage.co/query', params)
+			api_response = api_result.json()
+			arr = list(api_response['Time Series (Daily)'].values())
+			Open = float(arr[0]['1. open'])
+			high = float(arr[0]['2. high'])
+			low = float(arr[0]['3. low'])
+			close = float(arr[0]['4. close'])
+			volume = float(arr[0]['5. volume'])
+
+
+			o_c = Open - close
+			h_l = high - low
+
+			sum_21 = 0.0
+			for i in range(21):
+				sum_21 = sum_21 + float(arr[i]['4. close'])	
+			
+			avg_21 = round(sum_21/21,6)
+
+			sum_7 = 0.0
+			for i in range(7):
+				sum_7 = sum_7 + float(arr[i]['4. close'])
+			avg_7 = round(sum_7/7,6)
+
+			sum_sd = 0
+			for i in range(7):
+				sum_sd = sum_sd + (float(arr[i]['4. close']) - avg_7)**2
+			sd_7 = round(math.sqrt(sum_sd/7),6)
+
+			input = [[volume,o_c,h_l,avg_7,avg_21, sd_7]]
+			REL_model= pickle.load(open('C:\\Users\\user\\Desktop\\Projects\\Minor Project - 2\\Deployment\REL model.pkl','rb'))
+			prediction= round(REL_model.predict(input)[0],2)
+
+
+
+			return render_template('index.html', prediction_text='Prediction of next day Closing Price of Reliance Industries Ltd after date {} is Rs.{}'.format(list(api_response['Time Series (Daily)'].keys())[0],prediction))
+
+		elif(request.form.get(["Stock"][0])=='PFZ'):
+			params = {
+				'function': 'TIME_SERIES_DAILY',
+				'symbol': 'PFE',
+				'outputsize' :'full',
+				'apikey' : 'SD7E3STMW2WO613H'
+				#   'query': 'New York'
+			}
+
+			api_result = requests.get('https://www.alphavantage.co/query', params)
+			api_response = api_result.json()
+			arr = list(api_response['Time Series (Daily)'].values())
+			Open = float(arr[0]['1. open'])
+			high = float(arr[0]['2. high'])
+			low = float(arr[0]['3. low'])
+			close = float(arr[0]['4. close'])
+			volume = float(arr[0]['5. volume'])
+
+
+			o_c = Open - close
+			h_l = high - low
+
+			sum_21 = 0.0
+			for i in range(21):
+				sum_21 = sum_21 + float(arr[i]['4. close'])	
+			
+			avg_21 = round(sum_21/21,6)
+
+			sum_7 = 0.0
+			for i in range(7):
+				sum_7 = sum_7 + float(arr[i]['4. close'])
+			avg_7 = round(sum_7/7,6)
+
+			sum_sd = 0
+			for i in range(7):
+				sum_sd = sum_sd + (float(arr[i]['4. close']) - avg_7)**2
+			sd_7 = round(math.sqrt(sum_sd/7),6)
+
+			input = [[volume,o_c,h_l,avg_7,avg_21, sd_7]]
+			PFZ_model= pickle.load(open('C:\\Users\\user\\Desktop\\Projects\\Minor Project - 2\\Deployment\PFZ model.pkl','rb'))
+			prediction= round(PFZ_model.predict(input)[0],2)
+
+			return render_template('index.html', prediction_text='Prediction of next day Closing Price of Pfizer Inc. after date {} is ${}'.format(list(api_response['Time Series (Daily)'].keys())[0],prediction))
+		elif(request.form.get(["Stock"][0])=='none'):
+			return render_template('index.html', prediction_text='Please Select a Stock to predict')
+		else:
+			return render_template('index.html', prediction_text='Unable to predict for now..Sorry for inconvenience')
+	except:
+		return render_template('index.html', prediction_text='Please Check your Internet Connection')
+
+
+if __name__== "__main__":
+	appp.run()
